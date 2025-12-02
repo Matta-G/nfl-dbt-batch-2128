c0b45e927009e6b4116e7c3d82df5d2db026b556
Camille Labrit <camille.labrit@gmail.com>
2025-12-02 17:06:00 +0000

    update nom des équipes sur les Dataset players

--- models/staging/stg_nfl__players_rushing_2024.sql
+++ models/staging/stg_nfl__players_rushing_2024.sql
@@ -12,7 +12,41 @@ renamed as (
         rk,
         player,
         age,
-        team,
+         CASE team
+    WHEN 'SFO'  THEN '49ers'
+    WHEN 'TEN' THEN 'Titans'
+    WHEN 'NYJ' THEN 'Jets'
+    WHEN 'MIA' THEN 'Dolphins'
+    WHEN 'NOR'  THEN 'Saints'
+    WHEN 'KAN'  THEN 'Chiefs'
+    WHEN 'NWE'  THEN 'Patriots'
+    WHEN 'PHI' THEN 'Eagles'
+    WHEN 'CIN' THEN 'Bengals'
+    WHEN 'BUF' THEN 'Bills'
+    WHEN 'HOU' THEN 'Texans'
+    WHEN 'IND' THEN 'Colts'
+    WHEN 'CHI' THEN 'Bears'
+    WHEN 'LAR'  THEN 'Rams'
+    WHEN 'LAC' THEN 'Chargers'
+    WHEN 'NYG' THEN 'Giants'
+    WHEN 'TAM'  THEN 'Buccaneers'
+    WHEN 'LVR'  THEN 'Raiders'
+    WHEN 'GNB'  THEN 'Packers'
+    WHEN 'CLE' THEN 'Browns'
+    WHEN 'ARI' THEN 'Cardinals'
+    WHEN 'DEN' THEN 'Broncos'
+    WHEN 'PIT' THEN 'Steelers'
+    WHEN 'SEA' THEN 'Seahawks'
+    WHEN 'DAL' THEN 'Cowboys'
+    WHEN 'WAS' THEN 'Commanders'
+    WHEN 'DET' THEN 'Lions'
+    WHEN 'JAX' THEN 'Jaguars'
+    WHEN 'CAR' THEN 'Panthers'
+    WHEN 'BAL' THEN 'Ravens'
+    WHEN 'MIN' THEN 'Vikings'
+    WHEN 'ATL' THEN 'Falcons'
+    ELSE team
+END as team,
         pos,
         g,
         gs,
