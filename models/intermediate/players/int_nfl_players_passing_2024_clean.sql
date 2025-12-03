286e01195400a4ef94e76a98f6bac748ee33bbe9
Mat R <mramos.freelance@gmail.com>
2025-12-03 13:50:15 +0000

    CLEANING - Ajout des 2 nouvelles colonnes de KPIs dans le Player Rushing 2024 - Suppression des lignes en doublon pour les joueurs qui ont été dans 2 teams lors de la saisons 2024 (1 ligne gardée, la ligne agrégée)

---
+++ models/intermediate/players/int_nfl_players_passing_2024_clean.sql
@@ -0,0 +1,17 @@
+-- Identifier les lignes en doublon via le row number
+
+WITH row_nb_table AS(
+    SELECT
+    *,
+    ROW_NUMBER() OVER (PARTITION BY player_pk ORDER BY team) AS row_nb
+FROM {{ ref('stg_nfl__players_passing_2024') }}
+
+)
+
+-- Sélectionner uniquement une ligne, la ligne agrégée (team = 2TM) identifiée grâce au row number = 1
+
+SELECT
+    *
+FROM row_nb_table
+WHERE 
+    row_nb = 1
